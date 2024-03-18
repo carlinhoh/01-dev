f79dbddc7e4175a55d00c6fa3e8b7b75acb1597a
carlinhoh <carloslucianosa@gmail.com>
2024-03-18 15:11:00 -0300

    Fix 0167-two-sum-II-input-array-is-sorted.cs code and complexities

--- Code/Leetcode/csharp/0167-two-sum-II-input-array-is-sorted.cs
+++ Code/Leetcode/csharp/0167-two-sum-II-input-array-is-sorted.cs
@@ -1,24 +1,22 @@
 /*
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/1207490335/
 
+Time:O(N)
+Space:O(1)
 */
+
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.Length-1;
-
-        while(left < right){
-            int sum = numbers[left] + numbers[right];
-            if(sum > target){
+        while(numbers[left] + numbers[right] != target ){
+            if(numbers[left]+numbers[right] >  target){
                 right--;
             }
-            else if(sum < target){
+            else{
                 left++;
             }
-            else{
-                return new int[]{left+1, right+1};
-            }
         }
-
-        return default;
+        return new int[] {left+1, right+1};
     }
 }
