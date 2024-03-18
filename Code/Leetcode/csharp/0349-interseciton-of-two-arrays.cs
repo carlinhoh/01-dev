005a19b5ead435914172d2f218f7f5cc974d4a40
carlinhoh <carloslucianosa@gmail.com>
2024-03-18 13:16:20 -0300

    Fix space complexity

--- Code/Leetcode/csharp/0349-interseciton-of-two-arrays.cs
+++ Code/Leetcode/csharp/0349-interseciton-of-two-arrays.cs
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/intersection-of-two-arrays/submissions/1207362969/
 
 Time:O(N+M)
-Space: O(N), If the res Hashset does not count O(1)
+Space: O(M), If the res Hashset does not count O(1)
 */
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
