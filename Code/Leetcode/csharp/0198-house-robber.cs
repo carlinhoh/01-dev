265c00b52d776b733bd3271db406385ef62201fe
carlinhoh <carloslucianosa@gmail.com>
2024-04-18 15:43:54 -0300

    Fix 0198-house-robber.cs code

--- Code/Leetcode/csharp/0198-house-robber.cs
+++ Code/Leetcode/csharp/0198-house-robber.cs
@@ -1,5 +1,5 @@
 /*
-https://leetcode.com/problems/house-robber/submissions/1228919942/
+https://leetcode.com/problems/house-robber/submissions/1235990046/
 
 Time:O(n)
 Space:O(1)
@@ -7,23 +7,22 @@ Space:O(1)
 public class Solution {
     public int Rob(int[] nums) {
         int n = nums.Length;
-        
-        if(n==1){            
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
             return nums[0];
         }
-        if(n==2){            
-            return Math.Max(nums[0],nums[1]);
-        }
 
-        int robNext = nums[n-1];
-        int robNextPlusOne = 0;
-        
-        for(int i=n-2;i>=0;i--){
-            int current = Math.Max(robNext, robNextPlusOne+nums[i]);
-            robNextPlusOne = robNext;
-            robNext = current;
+        int robHouse = nums[0];
+        int robNextHouse = Math.Max(nums[0], nums[1]);
+        int max = robNextHouse;
+
+        for(int i=2;i<nums.Length;i++){
+            max = Math.Max(nums[i] + robHouse, robNextHouse);
+            robHouse = robNextHouse;
+            robNextHouse = max;
         }
-        
-        return robNext;
+        return max;
     }
 }
