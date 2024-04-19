57bc656f435a2d238ea659fb26a556f34f2aeb24
carlinhoh <carloslucianosa@gmail.com>
2024-04-19 13:20:17 -0300

    Add 0143-reorder-list.cs

---
+++ Code/Leetcode/csharp/0143-reorder-list.cs
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/reorder-list/submissions/1236693532/
+
+Time: O(n)
+Space: O(1)
+*/
+public class Solution {
+    public void ReorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null; 
+        ListNode prev = null;
+        while (second != null) {
+            ListNode tmp = second.next;
+            second.next = prev;
+            prev = second;
+            second = tmp;
+        }
+
+        ListNode first = head;
+        second = prev; 
+        while (second != null) {
+            ListNode tmp1 = first.next, tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
