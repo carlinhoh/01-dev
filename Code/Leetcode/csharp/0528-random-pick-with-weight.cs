11322d79b7e93a7ef597b9353a0fd7940e33e81c
carlinhoh <carloslucianosa@gmail.com>
2024-04-28 01:58:10 -0300

    Fix 0528-random-pick-with-weight.cs

---
+++ Code/Leetcode/csharp/0528-random-pick-with-weight.cs
@@ -0,0 +1,43 @@
+/*
+https://leetcode.com/problems/random-pick-with-weight/submissions/1243784370/
+
+Time: Constructor = O(n), PickIndex = O
+*/
+    public class Solution {
+
+    int totalSum;
+    int[] possibilities;
+    Random random;
+
+    public Solution(int[] w) {
+
+        possibilities = new int[w.Length];
+
+        for(int i=0;i<w.Length;i++){
+            totalSum += w[i];
+            possibilities[i] += totalSum;
+        }
+        random = new Random();
+    }
+
+    public int PickIndex() {
+
+        double randomTarget = totalSum * random.NextDouble();
+
+        int left = 0;
+        int right = possibilities.Length;
+
+        while(left<right){
+            int mid = left + (right - left)/2;
+
+            if(randomTarget > possibilities[mid]){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+
+        return left; 
+    }
+}
