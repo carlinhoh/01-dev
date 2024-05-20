0a074f6edf3bf7c986fb0b84485f16895c7e0c51
carlinhoh <carloslucianosa@gmail.com>
2024-05-19 22:29:08 -0300

    Add a new approach to 1216-valid-palindrome-iii.cs

--- Code/Leetcode/csharp/1216-valid-palindrome-iii.cs
+++ Code/Leetcode/csharp/1216-valid-palindrome-iii.cs
@@ -3,7 +3,37 @@ https://leetcode.com/problems/valid-palindrome-iii/submissions/1262662821/
 
 Time: O(n²)
 Space: O(n)
+
+Without reverse https://leetcode.com/problems/valid-palindrome-iii/submissions/1262666481/
+
+Time: O(n²)
+Space: O(n)
 */
+public class Solution {
+    public bool IsValidPalindrome(string s, int k) {
+        if(s.Length == k) return true;
+
+        int[] previous = new int[s.Length + 1];
+        int[] current = new int[s.Length + 1];
+
+        for(int i = s.Length - 1; i >= 0; i--) {
+            for(int j = 0; j < s.Length; j++) {
+                if(s[i] == s[j]) {
+                    current[j + 1] = previous[j] + 1;
+                } else {
+                    current[j + 1] = Math.Max(previous[j + 1], current[j]);
+                }
+            }
+
+            int[] temp = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return (s.Length - k) <= previous[s.Length];
+    }
+}
+
 public class Solution {
     public bool IsValidPalindrome(string s, int k) {
         if(s.Length == k) return true;
