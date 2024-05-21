7dd271bed324504fa2f7c4d2d9ce20332dbde353
carlinhoh <carloslucianosa@gmail.com>
2024-05-21 17:45:32 -0300

    Add new approach to 1650-lowest-common-ancestor-of-a-binary-tree-iii.cs

--- Code/Leetcode/csharp/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
+++ Code/Leetcode/csharp/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
@@ -1,9 +1,67 @@
 /*
+Depth 
+
+Two pointers https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/submissions/1264344167/
+
+Time: O(h)
+Space: O(1)
+
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/submissions/1242833161/
 
 Time: O(n)
 Space: O(d), where d is the number of unique nodes in the path between p and q. O(n) in case of a degenerate (or pathological) tree (each parent node has only one associated child node).
 */
+
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+*/
+
+public class Solution {
+    public Node LowestCommonAncestor(Node p, Node q) {
+        int depthP = GetDepth(p), depthQ = GetDepth(q);
+
+        while(depthP > depthQ){
+            p = p.parent;
+            depthP--;
+        }
+        while(depthQ > depthP){
+            q = q.parent;
+            depthQ--;
+        }
+
+        while(q != p){
+            q = q.parent;
+            p = p.parent;
+        }
+        return q;
+    }
+
+    private int GetDepth(Node node){
+        int depth = 0;
+        while(node != null){
+            depth++;
+            node = node.parent;
+        }
+        return depth;
+    }
+}
+public class Solution {
+    public Node LowestCommonAncestor(Node p, Node q) {
+        Node p1 = p, p2 = q;
+        while(p1 != p2){
+            p1 = p1 == null ? p : p1.parent;
+            p2 = p2 == null ? q : p2.parent;
+        }
+        return p1;
+    }
+}
+
 public class Solution {
     public Node LowestCommonAncestor(Node p, Node q) {
 
