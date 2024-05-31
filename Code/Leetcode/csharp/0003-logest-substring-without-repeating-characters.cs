0b8475b0d46bea5791c6f6989a7a5471b3d16e64
carlinhoh <carloslucianosa@gmail.com>
2024-05-30 21:59:56 -0300

    Add 0003-logest-substring-without-repeating-characters.cs

--- Code/Leetcode/csharp/0003-logest-substring-without-repeating-characters.cs
+++ Code/Leetcode/csharp/0003-logest-substring-without-repeating-characters.cs
@@ -1,11 +1,27 @@
 /*
 #blind-75
 #neetcode-150
-https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1156837867/
-
+Dictionary https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1156837867/
+HashSet https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1272799758/
 Time:O(N)
 Space:O(K) Where K is defined by the number of distinct characters, in the case of alphabet: 26 the solution is O(1).
 */
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> seen = new();
+        int left = 0;
+        int max = 0;
+        for(int right=0;right<s.Length;right++){
+            while(seen.Contains(s[right])){
+                seen.Remove(s[left++]);
+            }
+            seen.Add(s[right]);
+            max = Math.Max(seen.Count, max);
+        }
+
+        return max;
+    }
+}
 
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
