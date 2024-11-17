9b8b3edb819e40261e2d912004466a304ce7cc02
carlinhoh <carloslucianosa@gmail.com>
2024-11-17 13:49:52 -0300

    Add another solution for 0155-min-stack

--- Code/Leetcode/csharp/0155-min-stack.cs
+++ Code/Leetcode/csharp/0155-min-stack.cs
@@ -38,6 +38,42 @@ public class MinStack {
     }
 }
 
+public class MinStack {
+    Stack<long> stack;
+    long min;
+    public MinStack() {
+        stack = new();
+    }
+    
+    public void Push(int val) {
+        if(stack.Count == 0){
+            stack.Push(0L);
+            min = val;
+        }
+        else{
+            stack.Push(val - min);
+            if(val < min){
+                min = val;
+            }
+        }
+    }
+    
+    public void Pop() {
+        if (stack.Count == 0) return;
+        long pop = stack.Pop();
+        if (pop < 0) min -= pop;
+    }
+    
+    public int Top() {
+        long top = stack.Peek();
+        return top > 0 ? (int)(top + min) : (int)(min);
+    }
+    
+    public int GetMin() {
+        return (int)min;
+    }
+}
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
