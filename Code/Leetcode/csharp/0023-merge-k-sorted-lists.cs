f83b9faf43e42c02738d49e88f5c9c7741fbcf2c
carlinhoh <carloslucianosa@gmail.com>
2024-11-24 21:41:01 -0300

    Add a new solution for 0023-merge-k-sorted-lists

--- Code/Leetcode/csharp/0023-merge-k-sorted-lists.cs
+++ Code/Leetcode/csharp/0023-merge-k-sorted-lists.cs
@@ -1,8 +1,12 @@
 /*
 https://leetcode.com/problems/merge-k-sorted-lists/submissions/1210340416/
 
-Time: O(Nlogk), where k\text{k}k is the number of linked lists.
-Space: O(N)
+Time: O(Nlogk), where k is the number of linked lists.
+Space: O(k)
+
+Merge Solution
+Time: O(NlogK)
+Space: O(1)
 */
 
 public class Solution {
@@ -30,5 +34,44 @@ public class Solution {
         }
 
         return head.next;
+    }
+}
+
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        if(lists.Length == 0){
+            return null;
+        }
+
+        int interval = 1;
+        while (interval < lists.Length){
+            for (int i = 0; i + interval < lists.Length; i += interval * 2){
+                lists[i] = Merge2Lists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+
+    public ListNode Merge2Lists(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode result = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val > l2.val){
+                dummy.next = l2;
+                dummy = dummy.next;
+                l2 = l2.next;
+            }
+            else{
+                dummy.next = l1;
+                dummy = dummy.next;
+                l1 = l1.next;
+            }
+        }
+        
+        dummy.next = l1 ?? l2;
+        
+        return result.next;
     }
 }
